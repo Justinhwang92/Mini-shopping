c784eed974fb374e604a73b7701bbef8d9dea9b5
Justin <justinhwang92@gmail.com>
2021-01-11 21:01:39 -0500

    fetch and display items

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// fetch the items from the JSON file
+function loadItems() {
+  return fetch("data/data.json")
+    .then((response) => response.json())
+    .then((json) => json.items);
+}
+
+function displayItesm(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+function createHTMLString(item) {
+  return `
+    <li class="item">
+      <img src="${item.image}" alt="${item.type}" class="item__thumnail" />
+      <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+// main
+loadItems()
+  .then((items) => {
+    //console.log(items);
+    displayItesm(items);
+    setEventListeners(items);
+  })
+  .catch(console.log);
